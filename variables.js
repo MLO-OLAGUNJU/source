22d6f4bf8d734a9b7846bd1d5ee8b4421809b957
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2023-12-07 01:01:51 +0100

    more lessons on code convention

--- variables.js
+++ variables.js
@@ -2,9 +2,19 @@
 // console.log(x);
 // x = 7;
 let x = 7;
+// X = 8;
 x = 6;
-x = 23;
-x = 34;
+x = 5;
+x = 4;
+// let x = 8;
 let y = 3;
 let z = x + y;
 console.log("Answer: " + z);
+
+// let let = 6
+let firstNumber = 7;
+let secondNumber = 3;
+let firstName = "Gold";
+let zipCode = "84763";
+
+let ZipCODE = "IY7788";
